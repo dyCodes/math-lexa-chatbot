12ff2375e61402b252d8b3f17e990313c15cf6f5
dyCodes <yusufdaudu51@gmail.com>
2022-09-09 00:55:53 +0100

    Add new changes

--- index.php
+++ index.php
@@ -2,52 +2,59 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-    integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./style.css" />
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=es6" class="script1"></script>
+    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3.0.1/es5/tex-mml-chtml.js"
+        class="script2"></script>
 
-  <link rel="stylesheet" href="./assets/style.css" />
-  <title>Math Lexa - ChatBot</title>
+    <title>Math Lexa - ChatBot</title>
 </head>
 
 <body>
-  <section class="chat-section">
-    <div class="chat-header">
-      <div class="image-box"><img src="./assets/images/lexa_2.png" /></div>
-      <div class="chat-title">
-        <h1>Math Lexa</h1>
-        <p>Ask me about the maths formulas you don't know.</p>
-      </div>
-    </div>
-
-    <div class="chat">
-      <div class="chat-box">
-        <div class="chat-box-image">
-          <img src="./assets/images/lexa_2.png" alt="" />
+    <section class="chat-section">
+        <div class="chat-header">
+            <div class="image-box"><img src="./assets/images/lexa_2.png" /></div>
+            <div class="chat-title">
+                <h1>Math Lexa</h1>
+                <p>Ask me about the maths formulas you don't know.</p>
+            </div>
         </div>
-        <div class="chat-box-inner-text">
-          <p>
-            Hi, 👋 I am Math-Lexa. Ask me about the maths formulas you don't
-            know.
-          </p>
+
+        <div class="chat">
+            <div class="chat-box">
+                <div class="chat-box-image">
+                    <img src="./assets/images/lexa_2.png" alt="" />
+                </div>
+                <div class="chat-box-inner-text">
+                    <p>
+                        Hi, 👋 I am Math-Lexa. Ask me about the maths formulas you don't
+                        know.
+                    </p>
+                </div>
+            </div>
+
         </div>
-      </div>
 
-    </div>
+        <div class="chat-input">
+            <form class="chat-form">
+                <input type="text" name="message" id="message" placeholder="Ask me your question here!" required />
+                <button id="send" type="submit">
+                    <i class="fa-regular fa-paper-plane"></i>
+                </button>
+            </form>
 
-    <div class="chat-input">
-      <input type="text" name="message" id="message" placeholder="Type your message here" />
-      <button id="send">
-        <i class="fa-regular fa-paper-plane"></i>
-      </button>
-    </div>
-  </section>
+        </div>
+    </section>
 
-  <script src="./assets/jquery.min.js"></script>
-  <script src="./assets/script.js"></script>
+    <script src="./jquery.min.js"></script>
+    <script src="./script.js"></script>
+
 </body>
 
 </html>
---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+  $("form.chat-form").submit(function (e) {
+    e.preventDefault();
+    var user_message = $("input#message").val();
+
+    if (user_message != "") {
+      $("div.chat").append(
+        '<div class="user-box"><div class="user"><p>' +
+          user_message +
+          "</p></div></div>"
+      );
+
+      $.ajax({
+        type: "POST",
+        url: "bot.php",
+        data: {
+          message: user_message,
+        },
+        cache: false,
+        success: function (data) {
+          $("div.chat").append(
+            '<div class="chat-box"><div class="chat-box-image"><img src="./assets/images/lexa_2.png" alt="" /></div><div class="chat-box-inner-text"><p>' +
+              data +
+              "</p></div></div>"
+          );
+
+          MathJax.typesetPromise();
+
+          // Scroll to reply
+          $(".chat").scrollTop($(".chat")[0].scrollHeight);
+
+          // Clear input
+          $("input#message").val("");
+        },
+      });
+    }
+  });
+});
+Mat;
